28562a9446aeb9672b9fa3cf8921bf7eefe0b10b
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-09-21 20:26:53 -0700

    display moods with button using javascript

--- scripts.js
+++ scripts.js
@@ -13,9 +13,26 @@ for (let i = 0; i < data.length; i += 1) {
 	img.src = data[i].image
 	img.width = 300
 	img.height = 300
-	// Add the image to the div
 	newDiv.appendChild(img)
-    
+
 	console.log(img) // Check the console!
 	itemsContainer.appendChild(newDiv)
+   
+   const desc = document.createElement ('p'); 
+
+   desc.innerText = data[i].desc
+   newDiv.appendChild(desc)
+
+   const price = document.createElement ('p')
+
+   price.innerText = data[i].price
+   newDiv.appendChild(price)
+
+   const button = document.createElement ('button')
+   button.id = data[i].name
+   button.dataset.price = data[i].price
+   button.innerHTML= "Add to Cart"
+   newDiv.appendChild(button)
+
+
 }
